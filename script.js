ca0330ed0ecd892b6613ab6ed829df7887840f43
ArthurHenriG <ArthurHenri00@outlook.com>
2025-02-26 22:09:59 -0300

    Updated HTML and JavaScript functions. The 'Clear' and 'Equal' buttons no longer have the 'button' class. Added clear() to reset the display and input. Implemented displayResult() to show the result.  operate() is still a draft and verifies the operation before returning a result. userInput() get the user input.

---
+++ script.js
@@ -0,0 +1,97 @@
+const btn=document.querySelectorAll('.button');
+const display=document.querySelector('.display');
+const input=document.querySelector('input');
+const btnClear=document.querySelector('.clear');
+const btnEqual=document.querySelector('.equal');
+
+function userInput(){
+    btn.forEach((btn)=>{
+        btn.addEventListener('click',(ele)=>{
+            input.value+=btn.value
+            operate(input.value);
+    }
+        ) 
+    })
+    
+}
+
+function displayResult(value){
+
+    btnEqual.addEventListener('click',()=>{
+       display.textContent=value;
+    })
+
+}
+
+function clear(){
+
+    btnClear.addEventListener('click',()=>{
+        input.value='';
+        display.innerHTML='';
+    })
+
+}
+
+clear()
+
+function operate(input){
+
+    let left;
+    let right;
+    let operator;
+    let result;
+    
+    for(let i=0;i<input.length;i++){
+        console.log('oi ' +input.slice(0,2));
+        if(input[i]=='/'){
+          left=input.slice(0,i);
+          right=input.slice(i+1,input.length);
+          operator=input[i];
+          break;
+        }else if(input[i]=='+'){
+            console.log(input[i])
+            left=parseFloat(input.slice(0,i));
+            right=parseFloat(input.slice(i+1,input.length));
+            operator=input[i];
+            result=sum(left,right);
+            break;
+        }else if(input[i]=='-'){
+            left=input.slice(0,i);
+            right=input.slice(i+1,input.length);
+            operator=input[i];
+            break;
+        }else if(input[i]=='*'){
+            left=input.slice(0,i);
+            right=input.slice(i+1,input.length);
+            operator=input[i];
+            break;
+        }else if(input[i]=='%'){
+           left=input.slice(0,i);
+           right=input.slice(i+1,input.length);
+           operator=input[i];
+           break;
+        }
+
+        console.log('fora ' +left,operator,right,input);
+
+}
+
+displayResult(result);
+        
+}
+
+function sum(leftNum,rightNum){
+    return leftNum+rightNum;
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
